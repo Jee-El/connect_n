e81da0fd623787e9f7e8755cff7051d25aa6fc54
Jee El <98971420+Jee-El@users.noreply.github.com>
2022-12-16 20:18:45 +0100

    Feat: implement #drop_disc

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,13 @@
 class Board
+  attr_reader :cols
 
+  def initialize
+    @cols = Array.new(7) { Array.new(6) }
+  end
+
+  def drop_disc(pick, color)
+    pick -= 1
+    row_index = @cols[pick].index(nil)
+    @cols[pick][row_index] = color if row_index
+  end
 end
