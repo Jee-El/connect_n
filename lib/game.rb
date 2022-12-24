0c397c88c2f61a053803655a9720d6d09a31983a
Jee El <98971420+Jee-El@users.noreply.github.com>
2022-12-24 20:58:38 +0100

    Place win methods into their own module

--- lib/game.rb
+++ lib/game.rb
@@ -1,49 +1,46 @@
 # frozen_string_literal: true
 
+require_relative 'winnable'
+
 class Game
+  include Winnable
+
   attr_reader :board, :players
 
   def initialize(board, current_player, other_player)
     @board = board
-
     @players = [current_player, other_player]
-
-    players.find { _1.instance_of?(ComputerPlayer) }&.game = self
   end
 
-  def start
+  def play
+    board.display
     loop do
-      board.display
+      current_player = players.first
 
-      pick = players.first.pick
+      pick = current_player.pick
 
       next puts 'Invalid pick' unless board.valid_pick?(pick)
 
-      color, col, row = board.drop_disc(pick, players.first.color)
-      break over(players.first) if over?(color, col, row)
+      color, col, row = board.drop_disc(pick, current_player.color)
+
+      clear_screen
+      board.display
+
+      break over(current_player) if over?(board, color, col, row)
 
       players.rotate!
     end
   end
 
-  def over?(color, col, row) = win?(color, col, row) || board.filled?
-
-  def win?(color, col, row)
-    (-1..1).any? do |k|
-      l = ((1..3).find { |i| board.at(col - i, row - k * i) != color } || 4) - 1
-      r = ((1..3).find { |i| board.at(col + i, row + k * i) != color } || 4) - 1
-      l + r >= 3
-    end || vertical_win?(color, col, row)
+  def over?(board, color = :nil, col = -Float::INFINITY, row = -Float::INFINITY)
+    win?(board, color, col, row) || board.filled?
   end
 
-  def over(winner)
-    board.display
-    puts "#{winner.name} has won!"
-  end
+  def over(winner) = puts "#{winner.name} has won!"
 
   private
 
-  def vertical_win?(color, col, row)
-    ((1..3).find { |i| board.at(col, row - i) != color } || 4) - 1 >= 3
+  def clear_screen
+    puts "\e[1;1H\e[2J"
   end
 end
