073cdd8bcbaa180cb3581ff8afac6807f641bf88
Jee El <98971420+Jee-El@users.noreply.github.com>
2022-12-27 00:47:35 +0100

    Explain how to play

--- lib/main.rb
+++ lib/main.rb
@@ -1,15 +1,31 @@
 # frozen_string_literal: true
 
+require 'tty-box'
+
 require_relative 'player/human_player/human_player'
 require_relative 'player/computer_player/computer_player'
 require_relative 'game/game'
 require_relative 'board/board'
 
-board = ConnectFour::Board.new
+board = ConnectFour::Board.new(cols_amount: 9, rows_amount: 9)
 
 human_player = ConnectFour::HumanPlayer.new
 computer_player = ConnectFour::ComputerPlayer.new(board, difficulty: 4)
 
 game = ConnectFour::Game.new(board, human_player, computer_player)
+
+main_screen_text = <<-TEXT
+  Welcome to Connect Four
+
+  To play, Enter a number from 1
+  to #{board.cols.length}
+  The number corresponds to the
+  column order starting from the
+  left.
+  Enter anything to proceed.
+TEXT
+puts TTY::Box.frame main_screen_text, padding: 2, align: :center
+gets
+puts "\e[1;1H\e[2J"
 
 game.play
