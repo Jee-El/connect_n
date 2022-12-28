b82067bfe414a2a7906d6eaf0906a74bcf8b8fcb
Jee El <98971420+Jee-El@users.noreply.github.com>
2022-12-28 23:58:18 +0100

    Add tests for the new HumanPlayer#pick

---
+++ spec/human_player_spec.rb
@@ -0,0 +1,38 @@
+require_relative '../lib/connect_four/connect_four'
+require_relative '../lib/player/human_player/human_player'
+
+describe ConnectFour::HumanPlayer do
+  subject(:human_player) { described_class.new }
+
+  describe '#pick' do
+    context 'when the user wants to save the game' do
+      let(:user_input) { ':W' }
+      before do
+        allow(ConnectFour::PROMPT).to receive(:ask).and_return(user_input)
+      end
+
+      it "returns :W downcased" do
+        pick = human_player.pick
+        expect(pick).to eq(user_input.downcase)
+      end
+    end
+
+    context 'when the user enters a column number' do
+      let(:user_input) { '4' }
+
+      before do
+        allow(ConnectFour::PROMPT).to receive(:ask).and_return(user_input)
+      end
+
+      it 'returns an Integer' do
+        input = human_player.pick
+        expect(input).to be_an(Integer)
+      end
+
+      it 'returns that number minus one' do
+        input = human_player.pick
+        expect(input).to eq(user_input.to_i - 1)
+      end
+    end
+  end
+end
