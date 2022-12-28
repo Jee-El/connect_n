8601ccfca61ac0e14e63c9a6b69099d23e32fa2b
Jee El <98971420+Jee-El@users.noreply.github.com>
2022-12-28 21:55:11 +0100

    Put display methods in module for better structure

---
+++ lib/displayable/displayable.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'tty-prompt'
+require 'tty-box'
+
+module ConnectFour
+  module Displayable
+    def welcome(board)
+      text = <<~TEXT
+        Welcome to Connect Four
+        #{'    '}
+        To play, Enter a number from 1
+        to #{board.table.columns_count}
+        The number corresponds to the
+        column order starting from the
+        left.
+        Enter anything to proceed.
+      TEXT
+      puts TTY::Box.frame text, padding: 2, align: :center
+      board.display
+    end
+
+    def invalid_pick
+      PROMPT.error 'Invalid Column Number'
+    end
+
+    def over(winner)
+      TTY::Box.success  "#{winner.name} has won!"
+    end
+
+    def clear_display
+      puts "\e[1;1H\e[2J"
+    end
+  end
+end
