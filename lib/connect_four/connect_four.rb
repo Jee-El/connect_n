410479793e6e9931649620ac9551cdeb793bcb26
Jee El <98971420+Jee-El@users.noreply.github.com>
2022-12-28 23:59:53 +0100

    Move require_relatives to main.rb

--- lib/connect_four/connect_four.rb
+++ lib/connect_four/connect_four.rb
@@ -1,11 +1,5 @@
 # frozen_string_literal: true
 
-require_relative '../player/human_player/human_player'
-require_relative '../player/computer_player/computer_player'
-require_relative '../game_setup/game_setup'
-require_relative '../game/game'
-require_relative '../board/board'
-
 module ConnectFour
   PROMPT = TTY::Prompt.new
 end
