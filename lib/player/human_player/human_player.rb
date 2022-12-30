9bc9271cec3bce2d33b4cc8547291199f73ecce4
Jee El <98971420+Jee-El@users.noreply.github.com>
2022-12-30 21:09:23 +0100

    Fix: empty input breaking #pick

--- lib/player/human_player/human_player.rb
+++ lib/player/human_player/human_player.rb
@@ -9,7 +9,7 @@ module ConnectFour
     def initialize(name: 'Human', disc: '🔥') = super
 
     def pick
-      input = PROMPT.ask('Please enter a column number : ').downcase
+      input = PROMPT.ask('Please enter a column number : ')&.downcase
       input == ':w' ? input : input.to_i - 1
     end
   end
