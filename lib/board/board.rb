61b3be250a45f694406963eef88f60a75bec4fdf
Jee El <98971420+Jee-El@users.noreply.github.com>
2022-12-30 21:06:07 +0100

    Refactor Board methods

--- lib/board/board.rb
+++ lib/board/board.rb
@@ -1,78 +1,92 @@
 # frozen_string_literal: true
 
-require 'tty-table'
-
 module ConnectFour
   class Board
-    attr_reader :table, :empty_disc
+    attr_reader :table, :empty_disc, :cols_amount, :rows_amount
 
     def initialize(cols_amount: 7, rows_amount: 6, empty_disc: '⚪')
       @empty_disc = empty_disc
 
-      @headers = TTY::Table.new rows: [[*'1 '.."#{cols_amount} "]]
+      @cols_amount = cols_amount
 
-      @rows = Array.new(rows_amount) { Array.new(cols_amount) { empty_disc } }
+      @rows_amount = rows_amount
 
-      @table = TTY::Table.new rows: @rows
+      @table = Array.new(rows_amount) { Array.new(cols_amount) { empty_disc } }
     end
 
     def initialize_copy(original_board)
       super
       @empty_disc = @empty_disc.clone
-      @rows = @rows.clone.map(&:clone)
-
-      @headers = Marshal.load Marshal.dump @headers
-      @table = Marshal.load Marshal.dump @table
+      @table = @table.clone.map(&:clone)
     end
 
     def drop_disc(disc, at_col:)
-      row = table.column(at_col).index(empty_disc)
-      return unless row
-
-      update(row, at_col, disc)
+      i = col(at_col).index(empty_disc)
+      row(i)[at_col] = disc
+      [i, at_col, disc]
     end
 
     def valid_pick?(pick)
-      !!table.column(pick)&.include?(empty_disc)
+      valid_col?(pick) and col(pick).include?(empty_disc)
     end
 
     def filled?
-      !table.flatten.index(empty_disc)
+      !table.flatten.include?(empty_disc)
     end
 
-    def display
-      # The table starts counting rows from top to bottom
-      # And since all the other methods rely on rows starting
-      # from bottom to top, it's easier to reverse the table rows
-      # when displaying it than doing rows.length - index everywhere.
-      table.rows.reverse!
-      puts table.render :ascii
-      table.rows.reverse!
-      puts @headers.render :ascii
+    def draw
+      table.each{ |row| draw_border || draw_row(row) }
+      draw_border
+      draw_col_nums
     end
 
-    def at(row, col)
-      # table[i, j] fails if :
-      # either argument is negative, or
-      # i is positive and a valid index while j is not valid
-      # table[i, j] returns nil if :
-      # both args are positive but both out of index bounds, or
-      # the first arg is out of index bounds
-      return if [row, col].any?(&:negative?) || !valid_pick?(col)
+    def cell(i, j)
+      return unless valid_cell?(i, j)
 
-      table[row, col]
+      row(i)[j]
+    end
+
+    def cols = table.transpose.map(&:reverse)
+
+    def rows = table
+
+    def col(j)
+      return unless valid_col?(j)
+
+      table.transpose[j].reverse
+    end
+
+    def row(i)
+      return unless valid_row?(i)
+
+      i = rows_amount - 1 - i
+      table[i]
     end
 
     private
 
-    def update(row, col, disc)
-      # tty-table seems to have an issue that makes it render
-      # the initialized table only, which means any changes
-      # made to it after initialization don't get rendered,
-      # so I have to create a new table with the new changes.
-      @rows[row][col] = disc
-      @table = TTY::Table.new rows: @rows
-      [row, col, disc]
+    def draw_border
+      cols_amount.times { print '+----' } and puts '+'
     end
+
+    def draw_row(row)
+      puts '| ' + row.join(' | ') + ' |'
+    end
+
+    def draw_col_nums
+      print '|'
+      (1..cols_amount).each do |num|
+        num.even? ? print(' ') : print('  ')
+        print num
+        num.odd? ? print(' ') : print('  |')
+      end
+      puts
+    end
+
+    def valid_cell?(i, j) = valid_row?(i) && valid_col?(j)
+
+    def valid_row?(i) = i.between?(0, rows_amount - 1)
+      
+    def valid_col?(j) = j.between?(0, cols_amount - 1)
   end
 end
