a57ae696b604a5e6cb1e8d861f046bb0b257f52c
Jee El <98971420+Jee-El@users.noreply.github.com>
2022-12-31 01:51:06 +0100

    Fix: #win? never returning true if min_to_win>4

--- lib/connect_n/winnable/winnable.rb
+++ lib/connect_n/winnable/winnable.rb
@@ -4,8 +4,8 @@ module ConnectN
   module Winnable
     def win?(board, row, col, disc)
       (-1..1).any? do |k|
-        l = ((1..3).find { |i| board.cell(row - k * i, col - i) != disc } || min_to_win) - 1
-        r = ((1..3).find { |i| board.cell(row + k * i, col + i) != disc } || min_to_win) - 1
+        l = ((1...min_to_win).find { |i| board.cell(row - k * i, col - i) != disc } || min_to_win) - 1
+        r = ((1...min_to_win).find { |i| board.cell(row + k * i, col + i) != disc } || min_to_win) - 1
         l + r + 1 >= min_to_win
       end || vertical_win?(board, row, col, disc)
     end
@@ -13,7 +13,7 @@ module ConnectN
     private
 
     def vertical_win?(board, row, col, disc)
-      (((1..3).find { |i| board.cell(row - i, col) != disc } || min_to_win)) >= min_to_win
+      (((1...min_to_win).find { |i| board.cell(row - i, col) != disc } || min_to_win)) >= min_to_win
     end
   end
 end
