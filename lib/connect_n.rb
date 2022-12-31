e95ff907c0ed59d35a44d57ebec94e88a273b488
Jee El <98971420+Jee-El@users.noreply.github.com>
2022-12-31 21:42:11 +0100

    Update paths

--- lib/connect_n.rb
+++ lib/connect_n.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-require_relative 'prompt/prompt'
-require_relative 'player/human_player/human_player'
-require_relative 'player/computer_player/computer_player'
-require_relative 'game_setup/game_setup'
-require_relative 'game/game'
-require_relative 'board/board'
+require_relative 'connect_n/prompt/prompt'
+require_relative 'connect_n/player/human_player/human_player'
+require_relative 'connect_n/player/computer_player/computer_player'
+require_relative 'connect_n/demo/demo'
+require_relative 'connect_n/game/game'
+require_relative 'connect_n/board/board'
 
 module ConnectN; end
